5e4bbba260dbcd0a05f138c6b9e7feadbf3dfe93
unknown <adamplishka@hotmail.com>
2025-01-11 19:27:48 -0500

    Initial Commit

---
+++ money-management-tools/src/main/java/com/plishka/utilities/money_management_tools/MoneyManagementToolsApplication.java
@@ -0,0 +1,13 @@
+package com.plishka.utilities.money_management_tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyManagementToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyManagementToolsApplication.class, args);
+	}
+
+}
---
+++ money-management-tools/src/test/java/com/plishka/utilities/money_management_tools/MoneyManagementToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plishka.utilities.money_management_tools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyManagementToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
